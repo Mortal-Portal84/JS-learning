c0519ad4dc2debb6155727791c52e9bbab4c78a2
Andrei <71920122+Mortal-Portal84@users.noreply.github.com>
2021-02-21 22:31:55 +0300

    repaired script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log(1);
+"use strict";
+
+const film = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+const personalMovieDb = {
+    count: 'film',
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const q1 = prompt('Один из последних просмотренных фильмов', ''),
+      a1 = +prompt('Сколько фильмов вы уже посмотрели', ''),
+      q2 = prompt('Один из последних просмотренных фильмов', ''),
+      a2 = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+personalMovieDb.movies[q1] = a1;
+personalMovieDb.movies[q2] = a2;
